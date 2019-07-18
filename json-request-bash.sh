c397764ced56d1d5d75e332c86f019428a1c71fd
Victor Pessolato <victor.pessolato@gmail.com>
2019-07-18 12:28:14 +0200

    Create json-request-bash.sh

---
+++ json-request-bash.sh
@@ -0,0 +1,19 @@
+# Replace the PROJECT_ID text for your project ID 
+
+JSON='{
+  "steps":
+  [
+    {
+      "name": "ubuntu",
+      "args":
+      ["echo", "$_EXAMPLE_USER_VARIABLE"
+      ]
+    }
+  ],
+  "substitutions":
+  {
+    "_EXAMPLE_USER_VARIABLE": "'$_EXAMPLE_USER_VARIABLE"
+  }
+}'
+
+curl -X POST -d "$JSON" -H "Content-Type: application/json" -H "Authorization: Bearer $(gcloud config config-helper --format='value(credential.access_token)')" https://cloudbuild.googleapis.com/v1/projects/[PROJECT_ID]/builds
